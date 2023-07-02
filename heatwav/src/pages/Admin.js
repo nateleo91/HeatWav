2ca0bdf0dde8965aaef94ac9c8b7faf25079c065
Nathen <leonard91j@gmail.com>
2023-07-02 13:32:57 -0400

    admin page changes

--- heatwav/src/pages/Admin.js
+++ heatwav/src/pages/Admin.js
@@ -5,9 +5,9 @@ function Admin() {
   const [selectedAdmin, setSelectedAdmin] = useState(null);
 
   const admins = [
-    { id: 1, name: 'Admin 1', bio: 'Bio for Admin 1', image: 'admin1.jpg', additionalInfo: 'Additional info for Admin 1' },
-    { id: 2, name: 'Admin 2', bio: 'Bio for Admin 2', image: 'admin2.jpg', additionalInfo: 'Additional info for Admin 2' },
-    { id: 3, name: 'Admin 3', bio: 'Bio for Admin 3', image: 'admin3.jpg', additionalInfo: 'Additional info for Admin 3' },
+    { id: 1, name: 'Admin 1', bio: 'Bio for Admin 1' },
+    { id: 2, name: 'Admin 2', bio: 'Bio for Admin 2' },
+    { id: 3, name: 'Admin 3', bio: 'Bio for Admin 3' },
     // Add more admins as needed
   ];
 
@@ -29,14 +29,8 @@ function Admin() {
       <div className="admin-bio">
         {selectedAdmin && (
           <div>
-            <div className="admin-image">
-              <img src={admins.find((admin) => admin.id === selectedAdmin).image} alt="Admin" />
-            </div>
-            <div className="admin-bio-content">
-              <h2>{admins.find((admin) => admin.id === selectedAdmin).name}</h2>
-              <p>{admins.find((admin) => admin.id === selectedAdmin).bio}</p>
-              <p>{admins.find((admin) => admin.id === selectedAdmin).additionalInfo}</p>
-            </div>
+            <h2>{admins.find((admin) => admin.id === selectedAdmin).name}</h2>
+            <p>{admins.find((admin) => admin.id === selectedAdmin).bio}</p>
           </div>
         )}
       </div>
